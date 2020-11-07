1247aeae08ab479ff025352c3186fa8183e2414a
Dmitry Stoletov <irvis@imega.ru>
2020-11-07 18:50:39 +0300

    add prefixexp extractor
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- formatter/chain_statments.go
+++ formatter/chain_statments.go
@@ -16,7 +16,7 @@ func (cs *chainStatments) Reset() {
 // 	cs.chain = append([]statementIntf{st}, cs.chain...)
 // }
 
-func (cs *chainStatments) Prev() statementIntf {
+func (cs *chainStatments) ExtractPrev() statementIntf {
 	if len(cs.chain) == 0 {
 		return nil
 	}
@@ -30,10 +30,46 @@ func (cs *chainStatments) Prev() statementIntf {
 	return cs.chain[len(cs.chain)-1]
 }
 
+func (cs *chainStatments) Prev() statementIntf {
+	if len(cs.chain) == 0 {
+		return nil
+	}
+
+	return cs.chain[len(cs.chain)-1]
+}
+
 func (cs *chainStatments) Len() int {
 	return len(cs.chain)
 }
 
 func (cs *chainStatments) First() statementIntf {
 	return cs.chain[0]
+}
+
+func (cs *chainStatments) ExctractPrefixexp() statementIntf {
+	item := -1
+	for i := len(cs.chain) - 1; i >= 0; i-- {
+		if _, ok := cs.chain[i].(*prefixexpStatement); ok {
+			item = i
+		}
+	}
+
+	if item == -1 {
+		return nil
+	}
+
+	v, _ := cs.chain[item].(*prefixexpStatement)
+	cs.chain = cs.chain[:item]
+
+	return v
+}
+
+func (cs *chainStatments) GetLastBody() statementIntf {
+	for i := len(cs.chain) - 1; i >= 0; i-- {
+		if v, ok := cs.chain[i].(*body); ok {
+			return v
+		}
+	}
+
+	return nil
 }
